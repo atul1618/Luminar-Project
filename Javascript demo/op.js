64a5e15adda6b5eef1acd957fbb854257aefc803
Atul Earackal <atulearackal@gmail.com>
2020-05-21 10:53:32 +0530

    Javascript commit

---
+++ Javascript demo/op.js
@@ -0,0 +1,8 @@
+//This is a single line comment
+var num1=Number(prompt("Num1: "));
+var num2=Number (prompt("Num2: "));
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1%num2);
